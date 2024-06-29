d2d88b7c663da5ae38b1b7a3127253f0ad3f1be9
dhikki-study <dhikki.study@gmail.com>
2024-06-29 14:52:25 -0400

    completed DP-1 after class

--- Sample.py
+++ Sample.py
@@ -1,51 +1,109 @@
-##############198. House Robber####################
-
-// Time Complexity : N
-// Space Complexity : N
+##############198. House Robber#
+#######################################################################################################################################################################
+// Time Complexity : done in 2 ways - recursion: 2^(number of houses) and matrix: number of houses
+// Space Complexity : recursion: not sure for recursion ad matrix: n
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : No
+// Any problem you faced while coding this : solved after class, so have better idea
 
 
 // Your code here along with comments explaining your approach in three sentences only
-//I created another list with running sum but it was with some tweaks. When I visit first element I check what is the maximum value when I moved to 2nd element I can't take value from i-1 (i.e. 1st element) so found max I can rob. When I moved to 3rd element (can't rob from 2nd) so what is the max I can get from elements <=i-1. Once the list is built just return max from list
+1 way used is recursion and another is once brute force place is played out implemented matrix 
 
-For input 
-[1,2,3,1] -> my list is [0, 1, 2, 4, 3]
-[2,7,9,3,1] -> my list is [0, 2, 7, 11, 10, 12]
-
-
+##############recursion###############
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        lmax=[0]
-        #tmp=[0]
-        for i in range(len(nums)):		# creating array with rolling some with tweaks
-            if i==0:
-                lmax.append((nums[i]))
-                #print("if:",lmax)
-            else:
-                #print("else:",i,lmax)
-                #print(lmax[:i])
-                tmp=lmax[:i]
-                vmax=max(tmp)
-                lmax.append(vmax+nums[i])
-        #print("final",lmax)
-        return max(lmax)			# find max for new array
+        return self.bruteforce(nums, 0)
+
+    def bruteforce(self,nums,idx):
+        #base
+        if idx>=len(nums):
+            return 0
+        #select
+        case1=nums[idx] + self.bruteforce(nums, idx+2)
+        #no select
+        case2=0 + self.bruteforce(nums, idx+1)
+        return max(case1,case2)
 
 
+###############Matrix##############
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        mat=[0]*(len(nums)+1)
+        mat[1]=max(mat[0],nums[0])
+        print(mat)
+        for i in range(2,len(nums)+1):
+            mat[i]=max(mat[i-1],nums[i-1]+mat[i-2])
+        print(mat)
+        return mat[-1]        
         
-        
 
-#############322. Coin Change###################
+#############322. Coin Change###################################################################################################################################################################################
 
 
-// Time Complexity : 
-// Space Complexity : 
-// Did this code successfully run on Leetcode : No
-// Any problem you faced while coding this : Not able to come up with good solution
+// Time Complexity : done in 2 ways - recursion: 2^(number of coins+target) and matrix: number of coins X target
+// Space Complexity : recursion: not sure for recursion ad matrix: mXn
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : solved after class, so have better idea
 
 
 // Your code here along with comments explaining your approach in three sentences only
-Not able to come up with any good solution though we could have done it in brute force but it would have been too long with multiple permutations and combinations
+1 way used is recursion and another is once brute force place is played out implemented matrix 
+
+##############Recursion##############
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        idx=0
+        ans=self.bruteforce(coins, amount, idx)
+        if ans==99999:
+            return -1
+        else:
+            return ans
+
+    def bruteforce(self,coins: List[int], amount: int, idx: int) -> int:
+        print(coins,'amount',amount,' index',idx)
+        #base case
+        if amount==0:
+            print('amount=0')
+            return 0
+        if amount<0 or idx==len(coins):
+            print('base else amount',amount,' coin index',idx)
+            return 99999
+
+        #logic
+            #select case
+        print('case 1 pre')
+        case1=1+self.bruteforce(coins,amount-coins[idx],idx)
+        print('case1 post')
+            #not select
+        case2=0+self.bruteforce(coins,amount,idx+1)
+        return min(case1,case2)
+
+
+        
+
+
+###############Matrix##############
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        m=amount
+        n=len(coins)
+        mat=[[99999 for j in range(m+1)] for i in range(n+1)]
+        mat[0][0]=0
+        #print(mat)
+        for i in range(1,n+1):
+            for j in range(m+1):
+                if j<coins[i-1]:
+                    mat[i][j]=mat[i-1][j]
+                else:
+                    mat[i][j]=min(mat[i-1][j],mat[i][j-coins[i-1]]+1)
+        #print(mat)
+        if mat[n][m]==99999:
+            return -1
+        else:
+            return mat[n][m]
+        
+        
 
 
 
