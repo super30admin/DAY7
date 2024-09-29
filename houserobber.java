a2ba96cc34c9f27da666a77108a38c4d81f22534
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-09-29 03:38:22 -0400

    Create houserobber.java

---
+++ houserobber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+        //if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        nums[1]=Math.max(nums[1],nums[0]);
+        for(int i=2;i<nums.length;i++){
+            nums[i]=Math.max(nums[i]+nums[i-2],nums[i-1]);
+        }
+        return nums[nums.length-1];
+    }
+}
