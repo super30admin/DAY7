d1eb3c68cac21cdc9392ac82cdf4416a28aff694
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-01-11 20:16:03 -0600

    Completed 2 problems

--- Sample.java
+++ Sample.java
@@ -2,8 +2,8 @@
 
 // Time Complexity : 0(n*m)
 // Space Complexity : o(n*m)
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 //Intuition is to create a matrix with all possible combinations to store solution
@@ -34,5 +34,56 @@ class Solution {
             return -1;
         }
         return dp[coins.length][amount];
+    }
+}
+
+
+/********************************* PROBLEM-2******************************/
+// Time Complexity : 2^n n=no of houses
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : no (time limit exceeded)
+// Any problem you faced while coding this : no
+
+//recursive approach
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0 || nums==null){
+            return 0;
+        }
+        return recurse(nums,0,0);
+    }
+
+    private int recurse(int[] nums,int index,int amount){
+        //base
+        if(index>=nums.length){
+            return amount;
+        }
+
+        //logic
+        int case0=recurse(nums,index+1,amount);
+        int case1=recurse(nums,index+2,amount+nums[index]);
+        return Math.max(case0,case1);
+    }
+}
+
+// Time Complexity : 0(n) n=no of houses
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums==null||nums.length==0){
+            return 0;
+        }
+
+        int[][] dp=new int[nums.length][2];
+        dp[0][1]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]=dp[i-1][0]+nums[i];
+        }
+
+        return Math.max(dp[nums.length-1][0],dp[nums.length-1][1]);
     }
 }
