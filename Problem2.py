ae2c2929b5c9b3d5e9a4cf2550d5fe34f733767d
PavanKS17 <pavansrinivas2@gmail.com>
2025-01-12 05:41:48 -0800

    DP - 1

--- Problem2.py
+++ Problem2.py
@@ -1,19 +1,17 @@
-#Similar to coin change, we need to use same dp table but use sum instead of min to store the number of ways of solving the problems, return the final element
-#TC: O(n*m)
-#SC: O(n*m)
-#Yes, this worked in leetcode
-#Same issue as coin change when used recursion
+#Keeping 2 columns skip and take and adding optimal solution until that row
+# TC: O(n)
+# SC: O(n)
+# Yes, this worked in leetcode
 
-def change(self, amount: int, coins: List[int]) -> int:
-    if len(coins) == 0:
-        return 0
-    dp = [[0 for _ in range(amount+1)] for _ in range(len(coins)+1)]
-    for i in range(len(coins) + 1):
-        dp[i][0] = 1
-    for i in range(1, len(coins) + 1):
-        for j in range(1, amount + 1):
-            if j < coins[i-1]:
-                dp[i][j] = dp[i-1][j]
-            else:
-                dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]]
-    return dp[len(coins)][amount]
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        n = len(nums)
+        skip = 0
+        take = nums[0]
+        for i in range(1, n):
+            temp = skip
+            skip = max(skip, take)
+            take = temp + nums[i]
+        return max(skip, take)
